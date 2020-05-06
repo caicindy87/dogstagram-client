7fab68fabb77f1a54aa537486c8bbb6108df86c2
Edward Mendoza <lalo.garibo90@gmail.com>
2020-05-05 17:46:36 -0700

    Completed create new dog and post

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+  checkExistingDog();
   fetchPosts();
+  createDog();
+  createPost();
 });
 
 function fetchPosts() {
@@ -9,7 +12,6 @@ function fetchPosts() {
 }
 
 function renderPosts(posts) {
-  console.log(posts);
   const card = document.getElementsByTagName("main")[0];
   posts.forEach((post) => {
     card.innerHTML += renderSinglePost(post);
@@ -42,4 +44,71 @@ function renderSinglePost(postInfo) {
       </form>
     </div>
   </div>`;
+}
+
+function createDog() {
+  const dogForm = document.querySelector(".dog-form");
+  const dogName = document.getElementById("dog-name-input");
+  const dogBreed = document.getElementById("dog-breed-input");
+
+  dogForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3000/api/v1/dogs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: dogName.value,
+        breed: dogBreed.value,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("dog_id", data.id);
+      });
+    dogForm.reset();
+  });
+}
+
+function createPost() {
+  const card = document.getElementsByTagName("main")[0];
+  const postForm = document.querySelector(".post-creation-form");
+  const captionInput = document.getElementById("caption-input");
+  const imageInput = document.getElementById("image-url-input");
+  const dogId = localStorage.getItem("dog_id");
+
+  postForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    fetch(`http://localhost:3000/api/v1/dogs/${dogId}/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        caption: captionInput.value,
+        image_url: imageInput.value,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        card.innerHTML += renderSinglePost(data);
+      })
+      .catch((err) => console.log(err.message));
+  });
+}
+
+function checkExistingDog() {
+  const dogFormContainer = document.querySelector(".dog-creation-form");
+  const postForm = document.querySelector(".post-creation-form");
+  let dogId = localStorage.getItem("dog_id");
+  if (dogId) {
+    dogFormContainer.style.display = "none";
+    postForm.style.display = "block";
+  } else {
+    dogFormContainer.style.display = "block";
+    postForm.style.display = "none";
+  }
 }
