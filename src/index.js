341cd37d193303c901fe0e65d1e9a309f1a6902e
caicindy87 <caicindy87@gmail.com>
2020-05-05 17:32:39 -0700

    finished liking feature. Also added ability to add new comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   fetchPosts();
   listenToLikeBtn();
+  listenToCommentSubmit();
 });
 
 function fetchPosts() {
@@ -17,35 +18,44 @@ function renderPosts(posts) {
 }
 
 function renderSinglePost(postInfo) {
-  return `<div class="container-fluid">
-    <div class="post-card">
-      <div class="title-section">
-        <span class="title">${postInfo.dog.name}</span>
-        <button class="edit-button">Edit</button>
-      </div>
-      <img src="${postInfo.image_url}" alt="" class="image" />
-      <button class="like-button" data-id="${postInfo.id}">♥</button>
-      <p class="likes" id="${postInfo.id}">${postInfo.likes} Likes</p>
-      <p class="caption">${postInfo.caption}</p>
-      <ul class="comments">
-      </ul>
-      <form class="comment-form">
-        <input
-          class="comment-input"
-          type="text"
-          name="comment"
-          placeholder="Add a comment..."
-        />
-        <button class="comment-button" type="submit">
-          Post
-        </button>
-      </form>
+  const partOne = `<div class="container-fluid">
+  <div class="post-card">
+    <div class="title-section">
+      <span class="title">${postInfo.dog.name}</span>
+      <button class="edit-button">Edit</button>
     </div>
-  </div>`;
+    <img src="${postInfo.image_url}" alt="" class="image" />
+    <button class="like-button" data-id="${postInfo.id}">♥</button>
+    <p class="likes" id="${postInfo.id}">${postInfo.likes} Likes</p>
+    <p class="caption">${postInfo.caption}</p>
+    <p id="comments-title">Comments</p>`;
+
+  let partTwo = `<ul class="comments" data-comment-post-id="${postInfo.id}">`;
+  postInfo.comments.forEach((comment) => {
+    partTwo += `<li>${comment.content}</li>`;
+  });
+
+  const partThree = `</ul>
+  <form class="comment-form" data-form-id="${postInfo.id}">
+    <input
+      class="comment-input"
+      type="text"
+      name="comment"
+      placeholder="Add a comment..."
+      data-input-id="${postInfo.id}"
+    />
+    <button class="comment-button" type="submit">
+      Post
+    </button>
+  </form>
+</div>
+</div>`;
+  return partOne + partTwo + partThree;
 }
 
 function listenToLikeBtn() {
   const card = document.getElementsByTagName("main")[0];
+
   card.addEventListener("click", (event) => {
     if (event.target.className === "like-button") {
       const postId = event.target.dataset.id;
@@ -65,4 +75,45 @@ function listenToLikeBtn() {
       });
     }
   });
+}
+
+function listenToCommentSubmit() {
+  const card = document.getElementsByTagName("main")[0];
+
+  card.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (event.target.className === "comment-form") {
+      const commentInputValue = event.target.getElementsByClassName(
+        "comment-input"
+      )[0].value;
+      const postId = parseInt(
+        event.target.getElementsByClassName("comment-input")[0].dataset.inputId
+      );
+
+      fetch("http://localhost:3000/api/v1/comments", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          content: commentInputValue,
+          post_id: postId,
+        }),
+      })
+        .then((resp) => resp.json())
+        .then((comment) => appendComment(comment));
+    }
+  });
+}
+
+function appendComment(json) {
+  const postId = json.post.id;
+  const ul = document.querySelector(`[data-comment-post-id='${postId}']`);
+  const commentForm = document.querySelector(`[data-form-id='${postId}']`);
+  const li = document.createElement("li");
+  li.innerText = json.content;
+  ul.append(li);
+  commentForm.reset();
 }
